2a7343d0667c3e13284629d0ba35524fbe2cf6e6
wherculano <wagherculano@hotmail.com>
2019-04-17 00:28:38 -0400

    Corrigindo scripts

--- Desafio104.py
+++ Desafio104.py
@@ -1,32 +1,30 @@
-#Desafio104.py
-'''
+# Desafio104.py
+"""
 Crie um programa que tenha a função leiaInt(), que vai funcionar de forma
 semelhante a função input() do Python, so que fazendo a validação para aceitar
 apenas um valor numerico.
 Ex: n = leiaInt('Digite um n')
 
 n = leiaInt('Digite um numero: ')
-print(f"Você acabou de digitar o numero {n}")
->>> Digite um numero: 4
->>> Voce acabou de digitar o numero 4
-
->>> Digite um numero: 
->>> Erro: Digite um numero inteiro
-
->>> Digite um numero: w
->>> Erro: Digite um numero inteiro valido
-'''
-def leiaInt(msg='Digite um numero: '):
-        while True:
-                num = input(msg)
-                if num.isnumeric():
-                        return int(num)
-                        break
-                else:
-                        print('\[033[0;31mERRO: Digite um NUMERO valido!\033[m')
-	
+print(f'Você acabou de digitar o numero {n}')
+"""
 
 
-#Main
-n = leiaInt()
+def leiaInt(msg):
+    sair = False
+    valor = 0
+    while True:
+        num = input(msg)
+        if num.isnumeric():
+            valor = int(num)
+            sair = True
+        else:
+            print('\033[0;31mERRO: Digite um NUMERO valido!\033[m')
+        if sair:
+            break
+    return valor
+
+
+# Main
+n = leiaInt('Digite um numero: ')
 print(f'Voce digitou o numero {n}')
--- Desafio105.py
+++ Desafio105.py
@@ -1,5 +1,5 @@
-#Desafio105.py
-'''
+# Desafio105.py
+"""
 Faca um programa que tenha uma função notas() que pode receber varias
 notas de alunos e vai retornar um dicionario com as seguintes informações:
 - Quantidade de notas
@@ -12,34 +12,29 @@ Adicione também as docstrings da função:
  n: uma ou mais notas dos alunos (aceita varias)
  sit: valor opcional, indicado se deve ou não adicionar a situação
  return: dicionario com varias informações sobre a situação da turma
+	>>> notas(5.5, 9.5, 10, 6.5)
+	{'total': 4, 'maior': 10, 'menor': 5.5, 'media': 7.875}
 
-resp = notas(5.5, 9.5, 10, 6.5)
-print(resp)
->>> {'total': 4, 'maior' : 10, 'menor': 5.5, 'media': 7.875}
+	>>> notas(5.5, 9.5, 10, 6.5, sit = True)
+	{'total': 4, 'maior': 10, 'menor': 5.5, 'media': 7.875, 'situacao': 'BOA'}
 
-resp = notas(5.5, 9.5, 10, 6.5, sit = True)
-print(resp)
->>> {'total': 4, 'maior' : 10, 'menor': 5.5, 'media': 7.875, 'situação': 'BOA'}
+	>>> notas(3.5, 2, 6.5, sit = True)
+	{'total': 3, 'maior': 6.5, 'menor': 2, 'media': 4.0, 'situacao': 'RUIM'}
 
-resp = notas(3.5, 2, 6.5 sit = True)
-print(resp)
->>> {'total': 3, 'maior' : 6.5, 'menor': 2, 'media': 4.0, 'situação': 'RUIM'}
+"""
 
-'''
-def notas(*nt,sit=False):
-	dados = {}
-	dados['total'] = len(nt)
-	dados['maior'] = max(nt)
-	dados['menor'] = min(nt)
-	dados['media'] = sum(nt)/len(nt)
-	if sit:
-		if dados['media'] >= 7:
-			dados['situacao'] = 'BOA'
-		else:
-			dados['situacao'] = 'RUIM'
-		return dados
-	else:
-		return dados
-	
+
+def notas(*nt, sit=False):
+    dados = {'total': len(nt), 'maior': max(nt), 'menor': min(nt), 'media': sum(nt) / len(nt)}
+    if sit:
+        if dados['media'] >= 7:
+            dados['situacao'] = 'BOA'
+        else:
+            dados['situacao'] = 'RUIM'
+        return dados
+    else:
+        return dados
+
+
 resp = notas(5.5, 9.5, 10, 6.5)
 print(resp)
