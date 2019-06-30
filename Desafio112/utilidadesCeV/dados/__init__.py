7a0844646e61f86ffdedd89c05e1a5d913f8db23
Herculano <wagherculano@hotmail.com>
2019-06-30 06:40:01 -0300

    Incluindo loop infinito ate ser digitado algo válido

--- Desafio112/utilidadesCeV/dados/__init__.py
+++ Desafio112/utilidadesCeV/dados/__init__.py
@@ -1,10 +1,10 @@
 def leiaDinheiro(msg):
-    valor = input(msg)
-    try:
-        valor = float(valor.replace(',', '.'))
-        return valor
-    except ValueError:
-        print('\033[0;31m')
-        print(f'ERRO: {valor} é um valor inválido')
-        print('\033[m')
-        exit()
+    while True:
+        valor = input(msg).strip()
+        try:
+            valor = float(valor.replace(',', '.'))
+            return valor
+        except ValueError:
+            print('\033[0;31m')
+            print(f'ERRO: {valor} é um valor inválido')
+            print('\033[m')
